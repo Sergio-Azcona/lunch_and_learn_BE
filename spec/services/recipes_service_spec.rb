57603e2a6a4a262abaa083d3e4def5b6ce6644cb
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-03-04 18:08:29 -0500

    test/feat: RecipesService#recipe_index_by_country tested

--- spec/services/recipes_service_spec.rb
+++ spec/services/recipes_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Recipes Service' do
   describe 'return an index of recipes for a given country' do
-    it 'returns a successful response with the top level keys requested' do
+    it 'responds with expected keys and datatypes' do
       country = 'thailand'
       json_response = File.read('spec/fixtures/recipes_index_for_thailand.json')
       stub_request(:get, 'https://api.edamam.com/api/recipes/v2?')
@@ -16,21 +16,16 @@ RSpec.describe 'Recipes Service' do
       expect(response[:hits]).to be_an(Array)
 
       response[:hits].each do |recipes|
-        # require 'pry';binding.pry
         expect(recipes).to be_an(Hash)
         expect(recipes).to have_key(:recipe)
         
-        recipes[:recipe].each do |recipe|
-          require 'pry';binding.pry
-          expect(recipe).to have_key(:uri)
-        
+        expect(recipes[:recipe]).to have_key(:label)
+        expect(recipes[:recipe][:label]).to be_a(String)
+        expect(recipes[:recipe]).to have_key(:image)
+        expect(recipes[:recipe][:image]).to be_a(String)
+        expect(recipes[:recipe]).to have_key(:url)
+        expect(recipes[:recipe][:url]).to be_a(String)
       end
-
-
-      end
-
-
-
     end
   end
 end
