6a91ac90363145d118c5b9129646847053f8fa60
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-03-04 22:31:06 -0500

    test/feat: recipes response returned serialized

--- spec/requests/api/v1/recipes_spec.rb
+++ spec/requests/api/v1/recipes_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Recipes API Responses' do
 
-    describe 'search by country' do
-      it 'returns a successful response with the expected top level and attributes keys' do 
+  describe 'search by country' do
+    describe 'successful response' do      
+      it 'returns the expected top level and attributes keys' do 
         country = 'thailand'
         json_response = File.read('spec/fixtures/recipes_index_response_for_thailand.json')
         stub_request(:get, 'https://localhost:3000/api/v1/recipes?')
@@ -11,11 +12,48 @@ RSpec.describe 'Recipes API Responses' do
           .to_return(status: 200, body: json_response)        
         
         response =  JSON.parse(json_response, symbolize_names: true)
-          require 'pry';binding.pry
-          expect(response).to be_a(Hash)
 
+        # require 'pry';binding.pry
+        expect(response).to be_a(Hash)
+        expect(response).to have_key(:data)
+        expect(response[:data]).to be_a(Array)
 
+        response[:data].each do |recipe|
+          expect(recipe).to have_key(:id)
+          expect(recipe[:id]).to be_a(String)
+          expect(recipe[:id]).to eq('null')
+          
+          expect(recipe).to have_key(:type)
+          expect(recipe[:type]).to be_a(String)
+          expect(recipe[:type]).to eq('recipe')
+
+          expect(recipe).to have_key(:attributes)
+          expect(recipe[:attributes]).to be_a(Hash)
+
+          expect(recipe[:attributes]).to have_key(:title)
+          expect(recipe[:attributes]).to have_key(:url)
+          expect(recipe[:attributes]).to have_key(:country)
+          expect(recipe[:attributes]).to have_key(:image)
+        end
+      end
+
+      it 'attribute keys return the expected value datatype; country matches the value passed in' do
+        country = 'thailand'
+        json_response = File.read('spec/fixtures/recipes_index_response_for_thailand.json')
+        stub_request(:get, 'https://localhost:3000/api/v1/recipes?')
+          .with(query: {'country'=> country })
+          .to_return(status: 200, body: json_response)        
+        
+        response =  JSON.parse(json_response, symbolize_names: true)
+        
+        response[:data].each do |recipe|
+          expect(recipe[:attributes][:title]).to be_a(String)
+          expect(recipe[:attributes][:url]).to be_a(String)
+          expect(recipe[:attributes][:country]).to be_a(String)
+          expect(recipe[:attributes][:country]).to eq(country)
+          expect(recipe[:attributes][:image]).to be_a(String)
+        end
       end
     end
-
+  end
 end
