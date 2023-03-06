572d302b19e4684f9d8ceaaa239f599b32ccbd1b
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-03-05 21:33:09 -0500

    feat: RecipesController organized; CountriesFacades evaluating input

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,34 +1,9 @@
 class Api::V1::RecipesController < ApplicationController
   def index
     country = params[:country]
-
-  # require 'pry';binding.pry
-  #   if country.nil?
-  #     country = CountryService.return_random_country
-  #     recipies = RecipesFacade.country_search(country)
-  #     render json: RecipeSerializer.recipies_index_response(country, recipies)
-  #   else #country param  exists
-  #     CountryService.country_exist?(country)
-  #     if country.exists? #country DOES exist
-  #       recipies = RecipesFacade.country_search(country)
-  #         unless recipies.nil?
-  #           render json: RecipeSerializer.recipies_index_response(country, recipies)
-  #         else
-  #           render json: RecipeSerializer.no_recipes_found
-  #         end
-  #     else #country does NOT exist
-  #       render json: RecipeSerializer.recipies_index_response(country, recipies)
-  #     end
-  #   end
-  # end
-
-    if country.nil? 
-      valid_country = CountriesFacade.return_random_country
-    else
-      valid_country = CountriesFacade.country_exists?(country)
-    end
-    # require 'pry';binding.pry
-
+    
+    valid_country = CountriesFacade.validate_input?(country)
+    
     recipies = RecipesFacade.country_search(valid_country) if valid_country.present?
     
     if valid_country && recipies
@@ -36,6 +11,5 @@ class Api::V1::RecipesController < ApplicationController
     else
       render json: RecipeSerializer.no_recipes_found
     end
-    # require 'pry';binding.pry
   end
 end
--- app/facades/countries_facade.rb
+++ app/facades/countries_facade.rb
@@ -1,23 +1,30 @@
 class CountriesFacade
   def self.return_random_country
     data = CountryService.all_countries
-    # require 'pry';binding.pry
+
     country_name = data.map { |info| Country.new(info) } 
     country_name.sample.common_name
-    # require 'pry';binding.pry
   end
 
-  def self.country_exists?(country)
-    
-      return nil if country == ''
-    # else
-      data = CountryService.validate_name(country)
-      data.map { |info| Country.new(info) }.first.common_name if data.present? #mapping a helper method
-    # end
+  def self.country_exists?(country)    
+    return nil if country == ''
+
+    data = CountryService.validate_name(country)
+    data.map { |info| Country.new(info) }.first.common_name if data.present? #mapping a helper method
   end
+
+
+  def self.validate_input?(country)
+    # require 'pry';binding.pry
+    if country.nil? 
+      self.return_random_country 
+    else
+      self.country_exists?(country)
+    end
+  end
+
 
   # def create_country_object(data)
-  # data.map { |info| Country.new(info) }
+  #   data.map { |info| Country.new(info) }
   # end
-
 end
