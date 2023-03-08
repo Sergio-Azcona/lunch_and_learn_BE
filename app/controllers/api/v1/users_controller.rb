e973cc48a1d3a86474fe5404656e2a2b102d87e2
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2023-03-08 00:31:24 -0500

    feat: users#create fully functional

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,16 +6,17 @@ class Api::V1::UsersController < ApplicationController
     if new_user.save
       render json: UserSerializer.show_serialize(new_user)      
     else
-      # require 'pry';binding.pry
-      render json: ErrorSerializer.serialized_response(new_user.errors.messages.first.second, 422), status: 422
+      render json: ErrorSerializer.serialized_response(new_user.errors.messages.first.second, 400), status: 400
     end
     
     # begin 
-    #   new_user = User.create!(user_params)
-    #   require 'pry';binding.pry
+    #   new_user.save
+    #   # new_user = User.create!(user_params)
+    #   # require 'pry';binding.pry
     #   render json: UserSerializer.show_serialize(new_user) 
 
     # rescue => error
+    #   # require 'pry';binding.pry
     #   render json: ErrorSerializer.serialized_response(error, 422), status: 422
     # end
   end
