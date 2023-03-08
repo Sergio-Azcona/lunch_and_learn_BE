932f719321f789f98687f0f49efb615b47bbd31d
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2023-03-08 02:12:30 -0500

    test: feature model relationship and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
+  describe 'validations and relationship' do
     it { should validate_presence_of  :name }
     it { should validate_presence_of  :email }
     it { should validate_uniqueness_of :email }
+
+    it { should have_many(:favorites) }
+
   end
 end
